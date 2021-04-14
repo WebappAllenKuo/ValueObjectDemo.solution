a21ab50a5e1f940cb29a7f6fb2e3e17b23d9d6df
LillianLin <a888gogogo@yahoo.com.tw>
2021-04-14 13:40:46 +0800

    add test and Member class

---
+++ ValueObjectDemo.Tests/Tests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace ValueObjectDemo.Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void AssignStringAndGetNumber()
+        {
+            var model = new Member();
+            model.Height = "100.8";
+            
+            decimal? actual = model.HeightNumer;
+            
+            Assert.IsTrue(actual.HasValue);
+            Assert.AreEqual(100.8M, actual.Value);
+        }
+    }
+
+    public class Member
+    {
+        public string Height { get; set; }
+        public decimal? HeightNumer { get; set; }
+    }
+}
